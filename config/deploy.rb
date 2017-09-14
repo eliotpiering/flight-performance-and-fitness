3b3ecf4fce38930407c658d8013da9c026014b5b
Eliot Piering <efpierin@wustl.edu>
2017-09-14 16:01:48 -0500

    more changes to cap;

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.1'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-append :rbenv_map_bins, 'rake', 'gem', 'bundle', 'ruby', 'rails', 'puma', 'pumactl'
+append :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all
 
 
