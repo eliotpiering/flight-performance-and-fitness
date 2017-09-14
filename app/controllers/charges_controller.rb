713b7216de4babe53a5a4d58426445565ef3ea0b
Eliot Piering <efpierin@wustl.edu>
2017-09-14 16:12:08 -0500

    uncommenting stripe code;

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,20 +7,20 @@ class ChargesController < ApplicationController
     @event = Event.find(params.require(:event_id))
     amount = @event.amount
 
-    # customer = Stripe::Customer.create(
-    #   :email => params[:stripeEmail],
-    #   :source  => params[:stripeToken]
-    # )
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
 
-    # Stripe::Charge.create(
-    #   :customer    => customer.id,
-    #   :amount      => amount,
-    #   :description => "purchased ticket to event #{@event.title}",
-    #   :currency    => 'usd'
-    # )
+    Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => amount,
+      :description => "purchased ticket to event #{@event.title}",
+      :currency    => 'usd'
+    )
 
 
-    event_registration = EventRegistration.create!(event: @event, email: params[:stripEmail])
+    event_registration = EventRegistration.create!(event: @event, email: params[:stripeEmail])
 
     EventMailer.thanks_for_registering_for(event_registration).deliver_now
     EventMailer.someone_registered_for(event_registration).deliver_now
