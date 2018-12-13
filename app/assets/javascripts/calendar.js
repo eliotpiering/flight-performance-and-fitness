ecea6b779b052eba94d5c8e255d27b1f9cac1b7e
= <=>
2018-12-13 13:48:36 -0600

    changes to the schedule; adding option for overlapping schedule items

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,11 +2,24 @@ document.addEventListener("turbolinks:load", function() {
     $('.event-item').each(function(item) {
         var _this = $(this);
         var duration = parseFloat(_this.data('duration'));
-        var height = duration * 60;
+        var hourHeight = 60;
+        var height = duration * hourHeight;
         var padding = 5;
         _this.height(height - (padding));
         _this.css("margin-top" , padding);
         _this.css("margin-bottom" , padding);
+      if(_this.data('overlap')) {
+        _this.css('position', 'relative');
+        var moveDown = parseFloat(_this.data('overlap')) * hourHeight;
+        _this.css('top', moveDown);
+        _this.css('width', '75%');
+      }
+
+      if(_this.innerHeight() < (hourHeight) && _this.innerWidth() < 160) {
+        var color = _this.css('background-color');
+        _this.find('.event-description').css('color', color);
+      }
+
     });
 
   //weird hack for the bootstrap calendar
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,13 +1,14 @@
 class Schedule 
 
-  attr_accessor :start_time, :end_time, :name, :description, :class_name
+  attr_accessor :start_time, :end_time, :name, :description, :class_name, :overlap
 
-  def initialize(start_time, end_time, name, description="", class_name)
+  def initialize(start_time, end_time, name, description="", class_name, options)
     @start_time = start_time
     @end_time = end_time
     @name = name
     @description = description
     @class_name = class_name
+    @overlap = options.delete(:overlap)
   end
 
   def duration
@@ -33,44 +34,44 @@ class Schedule
     when :sunday
       []
     when :monday
-      [open_gym(day, 5.5, 4.5), personal_training(day, 10, 5), open_gym(day, 15, 5)]
+      [open_gym(day, 5.5, 5.5), personal_training(day, 11, 4), open_gym(day, 15, 5.5)]
     when :tuesday
-      [conditioning(day, 5.5), open_gym(day, 6.5, 2.5), conditioning(day, 9), personal_training(day, 10, 5), open_gym(day, 15, 2.5), conditioning(day, 17.5), open_gym(day, 18.5)]
+      [conditioning(day, 5.5), open_gym(day, 6.5, 2.5), conditioning(day, 9), personal_training(day, 10, 5), open_gym(day, 15, 3), conditioning(day, 18), open_gym(day, 19, 1.5)]
     when :wednesday
-      [open_gym(day, 5.5, 4.5), personal_training(day, 10, 5), open_gym(day, 15, 5)]
+      [open_gym(day, 5.5, 5.5), personal_training(day, 11, 4), open_gym(day, 15, 5.5)]
     when :thursday
-      [conditioning(day, 5.5), open_gym(day, 6.5, 2.5), conditioning(day, 9), personal_training(day, 10, 5), open_gym(day, 15, 2.5), conditioning(day, 17.5), open_gym(day, 18.5)]
+      [conditioning(day, 5.5), open_gym(day, 6.5, 2.5), conditioning(day, 9), personal_training(day, 10, 5), open_gym(day, 15, 3), conditioning(day, 18), open_gym(day, 19, 1.5)]
     when  :friday
-      [open_gym(day, 5.5, 4.5), personal_training(day, 10, 5), open_gym(day, 15, 5)]
+      [open_gym(day, 5.5, 5.5), personal_training(day, 11, 4), open_gym(day, 15, 5.5)]
     when :saturday
-      [empty(day, 5.5, 3), conditioning(day, 8.5), open_gym(day, 9.5, 0.5), boxing(day, 10)]
+      [empty(day, 5.5, 2.5), open_gym(day, 8, 7), conditioning(day, 8.5, 1, overlap: -6.5)] #, open_gym(day, 9.5, 3.5),]
     end
   end
 
-  def self.event(name, description="", class_name, date, start_hour, duration)
+  def self.event(name, description="", class_name, date, start_hour, duration, options)
     start = date + start_hour.hours
     end_time = start + duration.hours
-    new(start, end_time, name, description, class_name)
+    new(start, end_time, name, description, class_name, options)
   end
 
   def self.empty(date, start_hour, duration=1)
-    event("", "", "empty-event", date, start_hour, duration)
+    event("", "", "empty-event", date, start_hour, duration, {})
   end
 
   def self.open_gym(date, start_hour, duration=1)
-    event("Open Gym", "Drop in for a workout. Flight training staff will work with you towards your fitness goals", "open-gym", date, start_hour, duration)
+    event("Open Gym", "Drop in for a workout. Flight training staff will work with you towards your fitness goals", "open-gym", date, start_hour, duration, {})
   end
 
-  def self.conditioning(date, start_hour, duration=1)
-    event("Conditioning", "Small group, boot camp style conditioning class in a fun team environment.", "conditioning", date, start_hour, duration)
+  def self.conditioning(date, start_hour, duration=1, options={})
+    event("Conditioning", "Small group, boot camp style conditioning class in a fun team environment.", "conditioning", date, start_hour, duration, options)
   end
 
   def self.personal_training(date, start_hour, duration=1)
-    event("Personal Training", "Dedicated Personal Training. By Appointment Only", "personal-training", date, start_hour, duration)
+    event("Personal Training", "Dedicated Personal Training. By Appointment Only", "personal-training", date, start_hour, duration, {})
   end
 
   def self.boxing(date, start_hour, duration=1)
-    event("Boxing", "", "boxing", date, start_hour, duration)
+    event("Boxing", "", "boxing", date, start_hour, duration, {})
   end
 
 
