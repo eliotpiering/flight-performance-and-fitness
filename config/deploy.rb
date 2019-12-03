884b7b88a4ca832e026fc7447e46f60e03cb2c87
Eliot Piering <eliot@eliotpiering.com>
2019-12-02 19:10:38 -0600

    linking storage directory to be shared (should be updated when using s3)

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :nvm_map_bins, %w{node npm yarn}
 append :linked_files, "Flight_prod"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
