d0207062835f17857166d6faf346e34282f91187
Eliot Piering <eliot@eliotpiering.com>
2020-08-10 21:33:04 -0500

    Updated Schedule - covid training times and removed Sunday; Updated About Page;

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,4 @@
-class Schedule 
+class Schedule
 
   attr_accessor :start_time, :end_time, :name, :description, :class_name, :overlap
 
@@ -30,18 +30,40 @@ class Schedule
     when :sunday
       [empty(day, 5.5, 14.5)]
     when :monday
-      [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+      covid_training_day(day)
     when :tuesday
-      [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+      covid_training_day(day)
     when :wednesday
-      [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+      covid_training_day(day)
     when :thursday
-      [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+      covid_training_day(day)
     when  :friday
-      [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+      covid_training_day(day)
     when :saturday
-      [empty(day, 5.5, 1.5), open_gym(day, 7, 5), empty(day, 8.5, 8),]
+      [empty(day, 5, 3.5), training_session(day, 8.5), no_classes(day, 9.5, 0.5), training_session(day, 10), no_classes(day, 11, 0.5), training_session(day, 11.5)]
     end
+
+    # Pre COVID schedule
+    # when :monday
+    #   [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+    # when :tuesday
+    #   [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+    # when :wednesday
+    #   [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+    # when :thursday
+    #   [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+    # when  :friday
+    #   [open_gym(day, 5.5, 4.5), empty(day, 10, 5), open_gym(day, 15, 5)]
+    # when :saturday
+    #   [empty(day, 5.5, 1.5), open_gym(day, 7, 5), empty(day, 8.5, 8),]
+    # end
+  end
+
+  def self.covid_training_day(day)
+    [training_session(day, 5), no_classes(day, 6, 0.5), training_session(day, 6.5), no_classes(day, 7.5, 0.5),
+     training_session(day, 8), no_classes(day, 9, 0.5), training_session(day, 9.5), empty(day, 10.5, 5.5),
+     training_session(day, 15), no_classes(day, 16, 0.5), training_session(day, 16.5), no_classes(day, 17.5, 0.5),
+     training_session(day, 18), no_classes(day, 19, 0.5), training_session(day, 19.5)]
   end
 
   def self.event(name, description="", class_name, date, start_hour, duration, options)
@@ -50,12 +72,20 @@ class Schedule
     new(start, end_time, name, description, class_name, options)
   end
 
+  def self.no_classes(date, start_hour, duration=1)
+    event("", "", "empty-event", date, start_hour, duration, {})
+  end
+
   def self.empty(date, start_hour, duration=1)
     event("Closed", "Closed", "empty-event", date, start_hour, duration, {})
   end
 
   def self.open_gym(date, start_hour, duration=1)
     event("Open Gym", "Drop in for a workout. Flight training staff will work with you towards your fitness goals", "open-gym", date, start_hour, duration, {})
+  end
+
+  def self.training_session(date, start_hour, duration=1, options={})
+    event("Training session", "Open gym with a limited group size. By Appointment Only", "training-session", date, start_hour, duration, options)
   end
 
   def self.conditioning(date, start_hour, duration=1, options={})
