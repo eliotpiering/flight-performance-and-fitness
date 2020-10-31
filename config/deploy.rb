2a5101487674d2ca65e9177bac76bb4e35ff4875
Eliot Piering <eliot@eliotpiering.com>
2020-10-31 15:20:43 -0500

    bump ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:eliotpiering/flight-performance-and-fitness.git"
 
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.4.1'
+set :rbenv_ruby, '2.6.5'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
